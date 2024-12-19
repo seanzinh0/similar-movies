961a4cabbf38a6fe5df419c4cddbb47c60f7eb58
spichay <seanpichay1015@gmail.com>
2024-12-19 13:58:34 -0500

    complete front-end

--- public/js/script.js
+++ public/js/script.js
@@ -7,16 +7,22 @@ form.addEventListener("submit", (e) => {
     const title = document.querySelector("input").value.replace(/ /g, "+");
     fetch(`http://localhost:3000/movie?title=${title}`).then(res => res.json().then((data) => {
             if (data.error) {
-                movieDiv.textContent = data.error;
+                movieDiv.innerHTML = data.error;
+            } else if (data.results.length === 0) {
+                movieDiv.innerHTML = "Cannot find similar movies";
             } else {
+                movieDiv.innerHTML = "";
                 let result = '';
                 data.results.forEach(movie => {
-                    const {title, release_date, overview, vote_average} = movie;
+                    const {title, release_date, overview, vote_average, poster_path} = movie;
                     result += `<div class="movie-card">
+                                    <img src="https://image.tmdb.org/t/p/original${poster_path}" alt="poster image">
+                                    <div class="card-body">
                                     <h3>${title}</h3>
-                                    <p>${release_date}</p>
-                                    <p>${vote_average}</p>
-                                    <p>${overview}</p>
+                                    <p>Release Date: ${release_date}</p>
+                                    <p>Rating: ${vote_average}</p>
+                                    <p>Overview: ${overview}</p>
+                                    </div>
                                 </div>`
                 })
                 movieDiv.innerHTML = result;
